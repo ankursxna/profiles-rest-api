cf71e28e2181a0a1c3ef1c92f67b021b904cf0be
Ankur <ankur.sxn92@gmail.com>
2021-07-31 01:26:45 +0530

    Added HelloAPIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """Test API View"""
+
+    def get(self, reuest, format=None):
+        """Returns a list of APIView Features"""
+
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message':'Hello Ankur','an_apiview':an_apiview})
+
+        # Django REST API is expecting a Response object. Hence we are returning a Response object here.
+        # Response object is converted into JSON. As we know JSON file format is Key-Value pair means a dictonary format.
+        # Hence inside the Response, we are passing values in the form of dictonary.
